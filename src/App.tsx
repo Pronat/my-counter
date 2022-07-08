709b5d5d7066803a18281a1ba3fa2352cc4ba1e3
AlexPron <Fotolinza@gmail.com>
2022-07-08 14:06:06 +0300

    lesson1-4

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,55 @@ import {Counter} from "./components/Counter";
 type CountType = number
 
 function App() {
-  let [count, setCount] = useState<CountType>(0)
-  const addCounter = () => {
-    if (count < 5) {
-      count = Number(count) + 1
-      setCount(count)
+    const [value, setValue] = useState<number>(0)
+    const addNumber = () => {
+        setValue(value + 1)
     }
-    console.log(count)
-  }
+    const SetToLocalStorage = () => {
+        localStorage.setItem("counterValue", value.toString())
+    }
+    const GetFromLocalStorage = () => {
+        let getValue = localStorage.getItem("counterValue")
+        if (getValue) {
+            setValue(JSON.parse(getValue))
+        }
+    }
 
-  const resetCounter = () => {
-    count = 0
-    setCount(count)
-    console.log(count)
-  }
 
-  return (
-    <div className="App">
-    <Counter
-        addCounter={addCounter}
-        resetCounter={resetCounter}
-        count={count}
-    />
-    </div>
-  );
+    return (
+        <div>
+            <h1>{value}</h1>
+            <button onClick={addNumber}>add NUmber</button>
+            <button onClick={SetToLocalStorage}>SetToLocalStorage</button>
+            <button onClick={GetFromLocalStorage}>GetFromLocalStorage</button>
+        </div>
+    )
 }
+
+
+// let [count, setCount] = useState<CountType>(0)
+// const addCounter = () => {
+//   if (count < 5) {
+//     count = Number(count) + 1
+//     setCount(count)
+//   }
+//   console.log(count)
+// }
+//
+// const resetCounter = () => {
+//   setCount(0)
+//   console.log(count)
+// }
+//
+// return (
+//   <div className="App">
+//   <Counter
+//       addCounter={addCounter}
+//       resetCounter={resetCounter}
+//       count={count}
+//   />
+//   </div>
+// );
+// }
 
 export default App;
