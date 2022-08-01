0f319803a593abbaf5de607815e6da867893a0c9
AlexPron <Fotolinza@gmail.com>
2022-08-01 23:16:04 +0300

    lesson1-10

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,32 @@
 import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Counter} from "./components/Counter";
 
 type CountType = number
 
 function App() {
-//     const [value, setValue] = useState<number>(0)
-//
-//     const addValue = () => {
-//         setValue(value + 1)
-//     }
-//     const setLocalStorageHandler = () => {
-//         localStorage.setItem("addValue", JSON.stringify(value))
-//     }
-//     const getLocalStorageHandler = () => {
-//         let newGet = localStorage.getItem("addValue")
-//         if (newGet) {
-//         setValue(JSON.parse(newGet))
-//     }}
-//     const clearLocalStorageHandler = () => {
-//         localStorage.clear()
-//     }
-//     const removeItemLocalStorageHandler = () => {
-//         localStorage.removeItem("addValue")
-//     }
-//
-//
-//     return (
-//         <div>
-//             <h1>{value}</h1>
-//             <button onClick={addValue}>inc</button>
-//             {/*<button onClick={setLocalStorageHandler}>setLocalStorage</button>*/}
-//             {/*<button onClick={getLocalStorageHandler}>getLocalStorage</button>*/}
-//             {/*<button onClick={clearLocalStorageHandler}>clearLocalStorage</button>*/}
-//             {/*<button onClick={removeItemLocalStorageHandler}>removeItemLocalStorage</button>*/}
-//         </div>
-//     )
-//
-// }
 
 
-let [count, setCount] = useState<CountType>(0)
-const addCounter = () => {
-  if (count < 5) {
-    count = Number(count) + 1
-    setCount(count)
-  }
-  console.log(count)
-}
+    let [count, setCount] = useState<CountType>(0)
+    const addCounter = () => {
+        if (count < 100) {
+           let newCount = Number(count) + 1
+            setCount(newCount)
+        }
+        console.log(count)
+    }
 
-const resetCounter = () => {
-  setCount(0)
-  console.log(count)
-}
+    const resetCounter = () => {
+        setCount(0)
+        console.log(count)
+    }
 
-return (
-  <div className="App">
-  <Counter
-      addCounter={addCounter}
-      resetCounter={resetCounter}
-      count={count}
-  />
-  </div>
-);
+    return (
+        <div className="App">
+            <button>inc</button>
+            <button>reset</button>
+
+        </div>
+    );
 }
 
 export default App;
