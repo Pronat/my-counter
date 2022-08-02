10b1abd38152482fe5929fac4db85c1f2730bc8d
AlexPron <Fotolinza@gmail.com>
2022-08-02 20:36:26 +0300

    lesson1-16

---
+++ src/components/Buttons.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import s from "./Counter.module.css";
+
+type ButtonsPropsType = {
+    count: number
+    maxValue: number
+    reset: () => void
+    addNumber: () => void
+}
+
+const Buttons = (props: ButtonsPropsType) => {
+    return (
+        <div className={s.block}>
+            <div className={s.block2}>
+                <div className={s.scoreboard}>
+                    <text className={s.text}>{props.count}</text>
+                </div>
+                <div className={s.buttons}>
+                    <button onClick={props.addNumber} disabled={props.count === props.maxValue}>inc</button>
+                    <button onClick={props.reset} disabled={props.count === 0}>reset</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Buttons;
--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,26 +1,29 @@
 import React, {useState} from 'react';
 import s from './Counter.module.css'
+import Buttons from "./Buttons";
 
 const Counter = () => {
     const [count, setCount] = useState<number>(0)
+    const maxValue = 5
     const reset = () => {
         setCount(0)
     }
     const addNumber = () => {
         if (count < 5) {
-        setCount(count +1)
-    }}
+            setCount(count + 1)
+        }
+    }
+
+
+
     return (
-        <div className={s.block}>
-            <div className={s.block2}>
-                <div className={s.scoreboard}>
-                    <text className={s.text}>{count}</text>
-                </div>
-                <div className={s.buttons}>
-                    <button onClick={addNumber}>inc</button>
-                    <button onClick={reset}>reset</button>
-                </div>
-            </div>
+        <div>
+            <Buttons
+            count={count}
+            maxValue={maxValue}
+            reset={reset}
+            addNumber={addNumber}
+            />
         </div>
     );
 };
