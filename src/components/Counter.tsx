ccc3ef726e474c2ab2037438adc11b0c0380ad32
AlexPron <Fotolinza@gmail.com>
2022-08-02 15:53:39 +0300

    lesson1-14

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,10 +7,12 @@ const Counter = () => {
         <div className={s.block}>
             <div className={s.block2}>
                 <div className={s.scoreboard}>
-                    <button>{count}</button>
+                    <text className={s.text}>{count}</text>
                 </div>
-                <button>inc</button>
-                <button>reset</button>
+                <div className={s.buttons}>
+                    <button>inc</button>
+                    <button>reset</button>
+                </div>
             </div>
         </div>
     );
