3955d6c910eb0499f18746f6725f540c5f8527cc
AlexPron <Fotolinza@gmail.com>
2022-08-02 23:24:44 +0300

    lesson1-20

--- src/components/SetForCounter.tsx
+++ src/components/SetForCounter.tsx
@@ -17,8 +17,8 @@ const SetForCounter = (props: SetForCounterPropsType) => {
     return (
         <div className={s.block}>
             <div className={`${s.block2} ${s.setButtons}`}>
-                <input className={s.button} onChange={setMax} value={props.maxValue} type="text"/>
-                <input className={s.button} onChange={setMin} value={props.minValue} type="text"/>
+                <div>max value<input className={s.button} onChange={setMax} value={props.maxValue} type="text"/></div>
+                <div>min value<input className={s.button} onChange={setMin} value={props.minValue} type="text"/></div>
                 <button className={s.button}>set</button>
             </div>
         </div>
