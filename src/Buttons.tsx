985f54fc3ac0a769086d4226b73c77d557ca6e94
AlexPron <Fotolinza@gmail.com>
2022-08-25 23:07:28 +0300

    lesson1-34

--- src/Buttons.tsx
+++ src/Buttons.tsx
@@ -7,14 +7,23 @@ export type ButtonsType = {
 }
 export const Buttons: React.FC<ButtonsType> = (props) => {
     let [error, setError] = useState<string | null>(null)
-    const addNumberHandler = () => {props.addNumber()}
+
+    const addNumberHandler = () => {
+        if (props.count === 10) {
+            setError('Wrong input')
+        }   else {
+            props.addNumber()
+            setError(null)
+        }
+    }
     const resetHandler = () => {props.reset()}
     return (
         <div>
             <button>{props.count}</button>
+            <span className>{error}</span>
             <div>
-                <button onClick={addNumberHandler}>inc</button>
-                <button onClick={resetHandler}>reset</button>
+                <button disabled={props.count === 10} onClick={addNumberHandler}>inc</button>
+                <button disabled={props.count === 0} onClick={resetHandler}>reset</button>
             </div>
         </div>
     )
