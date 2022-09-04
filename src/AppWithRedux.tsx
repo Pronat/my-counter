f18081f1c7c8504f816ae2870ad51fe4c3ae7a6a
AlexPron <Fotolinza@gmail.com>
2022-09-04 21:39:54 +0300

    counterRedux-11

--- src/AppWithRedux.tsx
+++ src/AppWithRedux.tsx
@@ -4,16 +4,14 @@ import {Buttons} from "./Buttons";
 import {useDispatch, useSelector} from "react-redux";
 import {AppRootStateType} from "./state/store";
 import {addNumberAC} from "./state/counter-reducer";
-import {resetToMinAC} from "./state/minValue-reducer";
+
 
 
 export function AppWithRedux() {
-    // let [minValue, setMinValue] = useState<number>(1)
-    // let [maxValue, setMaxValue] = useState<number>(5)
+    let [minValue, setMinValue] = useState<number>(1)
+    let [maxValue, setMaxValue] = useState<number>(5)
     // const [count, setCount] = useState<number>(0)
     let count = useSelector<AppRootStateType, number>(state => state.counts)
-    let minValue = useSelector<AppRootStateType, number>(state => state.minValue)
-    let maxValue = useSelector<AppRootStateType, number>(state => state.maxValue)
     let dispatch = useDispatch()
     let [error, setError] = useState<string | null>(null)
 
@@ -23,9 +21,7 @@ export function AppWithRedux() {
         // setCount(count + 1)
     }
     const reset = () => {
-        let action = resetToMinAC(minValue)
-        dispatch(action)
-        // setCount(Number(minValue))
+        setCount(Number(minValue))
     }
 
 
--- src/state/minValue-reducer.test.ts
+++
@@ -1,9 +0,0 @@
-import MinValueReducer, {resetToMinAC} from "./minValue-reducer";
-
-
-test('Reset to min should be correct', () => {
-    let startState = 5
-    let minValue = 2
-    let endState = MinValueReducer(startState, resetToMinAC(minValue))
-    expect(endState).toBe(2)
-})
